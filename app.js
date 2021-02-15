ca155f339f6f2e2a26b70eb22ad469d9ae0621d8
KalebMcElyea <kalebmac@live.com>
2021-02-15 16:17:42 -0700

    Zookeeper

---
+++ app.js
@@ -0,0 +1,333 @@
+class Alligator {
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+
+}
+
+class Bear{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+    
+   
+}
+
+class Camel{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+   
+
+
+}
+
+class Dingo{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+
+}
+
+
+class Eagle{ 
+    constructor(name ,color, gender, size, weight){
+    this.name = name;
+    this.color = color;
+    this.gender=gender;
+    this.size=size;
+    this.weight=weight
+}
+}
+
+class Ferret{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Gecko{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Hippo{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Iguana{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Jaguar{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Kangaroo{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Lion{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Meerkat{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Numbat{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Otter{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Panther{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Quoll{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Rhino{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Snake{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Tiger{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Umberellabird {
+
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Vulture {
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Walrus {
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Xerus {
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Yak{
+    constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+class Zebra{
+     constructor(name ,color, gender, size, weight){
+        this.name = name;
+        this.color = color;
+        this.gender=gender;
+        this.size=size;
+        this.weight=weight
+    }
+}
+
+let adam = new Alligator("Adam", "Green", "M","Large", "200lbs" )
+
+let banjo = new Bear ("Banjo","Brown", "M", "Large", "150lbs" )
+
+let chester = new Camel ("Chester", "Tan", "M", "Small", "164lbs")
+
+let debby = new Dingo ("Debby", "Orange", "F", "Small", "55lbs")
+
+let eleanor = new Eagle ("Eleanor", "White", "F", "Extra-Small", "40lbs")
+
+let fanci = new Ferret ("Fanci", "Brown", "F", "Medium", "35lbs")
+
+let geico = new Gecko ("Geico", "Green", "M", "Small", "2lbs")
+
+let halsey = new Hippo ("Halsey", "Silver", "F", "Extra-Large", "763lbs")
+
+let iggy = new Iguana ("Iggy", "Green", "M", "Medium", "25lbs")
+
+let jaxson = new Jaguar ("Jaxson", "Black", "M", "Large","105lbs")
+
+let kanga = new Kangaroo ("Kanga", "Tan", "F", "Large", "208lbs")
+
+let leeumm = new Lion ("Leeumm", "Orange", "M", "Small", "178lbs")
+
+let maddog = new Meerkat ("Maddog", "White", "M", "Extra-Small", "20lbs")
+
+let nakazawa = new Numbat ("Nakazawa", "Brown", "M", "Medium", "10lbs")
+
+let o = new Otter ("O", "Brown", "M", "Small", "10lbs")
+
+let paddy = new Panther ("Paddy", "Black", "F", "Large", "196lbs")
+
+let qi = new Quoll ("Qi", "Black", "F", "Extra-Small", "2lbs")
+
+let raba = new Rhino ("Raba", "Silver", "M", "Extra-Large", "420lbs")
+
+let sade = new Snake ("Sade", "Green", "M", "Small", "35lbs")
+
+let tigger = new Tiger ("Tigger", "Orange", "M", "Medium", "143lbs")
+
+let ugg = new Umberellabird ("Ugg", "Black", "F", "Small", "37lbs")
+
+let val = new Vulture ("Val", "Black", "F", "Large", "170lbs" )
+
+let wabo = new Walrus ("Wabo", "Tan", "M", "Extra-Large", "397lbs")
+
+let xin = new Xerus ("Xin", "Brown", "M", "Small", "4lbs")
+
+let yeet = new Yak ("Yeet", "Brown", "M", "Medium", "217lbs")
+
+let zolt = new Zebra ("Zolt", "White and black stripped", "M", "Large", "186lbs")
+
+
+
+let mamals = [zolt, yeet, xin, wabo, tigger, raba, qi, paddy, o, nakazawa, maddog, leeumm, kanga,
+     jaxson, halsey, fanci, debby, chester, banjo];
+     console.log(mamals);
+
+     
+let reptiles = [sade, iggy, geico, adam]
+    console.log(reptiles);
+
+    let birds = [val, ugg, eleanor]
+        console.log(birds);
